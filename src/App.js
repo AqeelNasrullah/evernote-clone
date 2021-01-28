94ed06ff6a09260acc9e390156c1d2039c824516
Aqeel Nasrullah <aqeelnasrulah1997@gmail.com>
2021-01-28 14:29:31 +0500

    Created note detail page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/home/Home";
 import Favorites from "./components/home/Favorites";
+import NoteDetail from "./components/notes/NoteDetail";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
             <Navbar/>
             <Switch>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/favorites" component={Favorites} />
+                <Route path="/favorites" component={Favorites} />
+                <Route path="/note/:id" component={NoteDetail} />
             </Switch>
         </BrowserRouter>
     </div>
--- src/components/notes/NotesList.js
+++ src/components/notes/NotesList.js
@@ -2,6 +2,7 @@ import React from "react";
 import {deleteNote, toggleFav} from "../../store/actions/noteAction";
 import {useDispatch} from "react-redux";
 import moment from "moment";
+import {Link} from "react-router-dom";
 
 const Note = ({note}) => {
     const dispatch = useDispatch();
@@ -15,7 +16,7 @@ const Note = ({note}) => {
                 <i className="material-icons red-text" style={{cursor: 'pointer'}} onClick={toggleFavoriteHandler}>{favMarkUp}</i>
                 <i className="material-icons" style={{cursor: 'pointer'}} onClick={deleteNoteHandler}>delete</i>
             </div>
-            <h5>{note.title}</h5>
+            <Link to={"/note/" + note.id}><h5>{note.title}</h5></Link>
             <p className="truncate">{note.desc}</p>
             <p>{moment(note.createdAt.toDate()).fromNow()}</p>
             <div className="right-align">
