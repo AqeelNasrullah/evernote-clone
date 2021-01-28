37cc3b256f761d608b36c8725df4e7615e984d27
Aqeel Nasrullah <aqeelnasrulah1997@gmail.com>
2021-01-28 13:37:57 +0500

    Created favorites page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/home/Home";
+import Favorites from "./components/home/Favorites";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
             <Navbar/>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/favorites" component={Favorites} />
             </Switch>
         </BrowserRouter>
     </div>
